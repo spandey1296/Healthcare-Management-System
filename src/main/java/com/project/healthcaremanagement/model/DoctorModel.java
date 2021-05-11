a581843dd2d5eeca7e85ac221a91ac89c376fe2c
Ashish2030 <Kumarashish79924@gmail.com>
2021-05-11 11:50:41 +0530

    doctor's database

--- src/main/java/com/project/healthcaremanagement/model/DoctorModel.java
+++ src/main/java/com/project/healthcaremanagement/model/DoctorModel.java
@@ -1,17 +1,27 @@
 package com.project.healthcaremanagement.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
 public class DoctorModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
-    private String prescriptions;
+    private String name;
+    private String specialization;
 
 
 
